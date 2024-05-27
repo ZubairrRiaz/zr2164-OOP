43388d1f363ac2ccbcdc4886e2178027a16404ae
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-05-27 09:37:23 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+import inquirer from 'inquirer';
+class Person {
+    name1 = 'Extrovert';
+    name2 = 'introvert';
+    name3 = 'Mystery.';
+    async inputfunction() {
+        let answer = await inquirer.prompt({
+            name: 'input',
+            type: 'input',
+            message: 'Type 1 if you like to talk to others and type 2 if you would rather keep to yourself:',
+        });
+        if (answer.input == '1') {
+            console.log(`You are: ${this.name1}`);
+        }
+        else if (answer.input == '2') {
+            console.log(`You are: ${this.name2}`);
+        }
+        else {
+            console.log(`Yo are Still a ${this.name3}`);
+        }
+        let ans = await inquirer.prompt({
+            name: 'input2',
+            type: 'input',
+            message: 'What is your Name?'
+        });
+        console.log(`Your Name is: ${ans.input2} and your Perspnality type is ${this.name3}`);
+    }
+    ;
+}
+let p1 = new Person();
+console.log(await p1.inputfunction());
